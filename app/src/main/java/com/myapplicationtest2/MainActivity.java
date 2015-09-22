819390d19e75d2c9e7638afb60f3d89c859b40e2
Angel Solis <ansolis90@gmail.com>
2015-09-22 13:55:19 -0400

    new commit 3

--- app/src/main/java/com/myapplicationtest2/MainActivity.java
+++ app/src/main/java/com/myapplicationtest2/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //asdiashdoiahsd
+    //asdasjdoiajsoidja
+    //asdaosdioqwjiwquqjwieohqhe
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
