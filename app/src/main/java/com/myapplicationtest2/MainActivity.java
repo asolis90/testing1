b09b1e1c1543546317a3cfd47ae2785ce9856772
Angel Solis <ansolis90@gmail.com>
2015-09-27 00:25:23 -0400

    minor change feature   4  -  test

--- app/src/main/java/com/myapplicationtest2/MainActivity.java
+++ app/src/main/java/com/myapplicationtest2/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //change feature master
+        //change feature 4 -
         //asdasdeeeeeeeeee
         //sasdiauhiuqhwiuqhweuq
         //asdasd
