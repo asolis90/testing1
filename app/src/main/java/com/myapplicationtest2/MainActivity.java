6302836845f3fd4aa2a723f7f8e983d38f2c6919
Angel Solis <ansolis90@gmail.com>
2015-09-27 00:18:03 -0400

    minor change feature 4

--- app/src/main/java/com/myapplicationtest2/MainActivity.java
+++ app/src/main/java/com/myapplicationtest2/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        //change new feature 4
         return super.onOptionsItemSelected(item);
     }
 }
